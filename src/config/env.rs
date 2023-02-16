79fb4c5bf794728044ddc462f479b7652398abd5
Hju Kneyck Flores <hju.halcyondigital@gmail.com>
2023-02-16 11:25:03 +0800

    refactor: fetch DATABASE_URL from app config

--- src/config/env.rs
+++ src/config/env.rs
@@ -4,4 +4,5 @@ lazy_static! {
     pub static ref SECRET_KEY: String = dotenv_codegen::dotenv!("SECRET_KEY").to_owned();
     pub static ref REDIS_URL: String = dotenv_codegen::dotenv!("REDIS_URL").to_owned();
     pub static ref SMS_HOST: String = dotenv_codegen::dotenv!("SMS_HOST").to_owned();
+    pub static ref DATABASE_URL: String = dotenv_codegen::dotenv!("DATABASE_URL").to_owned();
 }
