8d33fda40008cdc6764c1261b15aa8b3f333ee51
Hju Kneyck Flores <hju.halcyondigital@gmail.com>
2023-02-16 09:17:10 +0800

    feat: enable expire time of 5 mins for each OTP

--- src/utils/redis.rs
+++ src/utils/redis.rs
@@ -1,28 +1,37 @@
-use redis::Commands;
+use redis::{aio::AsyncStream, AsyncCommands, Client};
+use tokio::macros::support::Pin;
 
 use crate::config::env::REDIS_URL;
 
 pub struct RedisClient {
-    con: redis::Connection,
+    pub client: Client,
+    pub con: redis::aio::Connection<Pin<Box<dyn AsyncStream + Send + Sync>>>,
 }
 
 impl RedisClient {
     pub async fn new() -> Result<Self, redis::RedisError> {
         let client = redis::Client::open(REDIS_URL.to_owned()).unwrap();
-        let con = client.get_connection()?;
+        let con = client.get_async_connection().await.unwrap();
 
-        Ok(Self { con })
+        Ok(Self { client, con })
     }
 
-    pub async fn set_key(&mut self, key: &str, value: &str) -> Result<(), redis::RedisError> {
-        self.con.set(key, value)
+    pub async fn set_key(
+        &mut self,
+        key: &str,
+        value: &str,
+    ) -> Result<Vec<String>, redis::RedisError> {
+        redis::cmd("SET")
+            .arg(&[key, value, "EX", "300"])
+            .query_async::<_, Vec<String>>(&mut self.con)
+            .await
     }
 
     pub async fn get_key(&mut self, key: &str) -> Result<String, redis::RedisError> {
-        self.con.get(key)
+        self.con.get(key).await
     }
 
     pub async fn del_key(&mut self, key: &str) -> Result<(), redis::RedisError> {
-        self.con.del(key)
+        self.con.del(key).await
     }
 }
