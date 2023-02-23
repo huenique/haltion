fdccbe78d9a82a097bf190531071007dcbea0e61
Hju Kneyck Flores <hju.halcyondigital@gmail.com>
2023-02-23 17:28:25 +0800

    refactor: update route jwt handler for reusability
    Extracted the logic for verifying the authorization header into a separate function to improve code organization and reusability. Renamed the function to verify_auth_header and updated the verify_jwt function to call the new function. Also updated the function signature to use the IntoResponse trait to make it more flexible for future use cases.

--- src/routes/jwts.rs
+++ src/routes/jwts.rs
@@ -1,14 +1,7 @@
 use crate::config::env::SECRET_KEY;
+use crate::services::jwts;
 use crate::structs::AppState;
-use crate::utils::jwt::Claims;
-use axum::{
-    body::Body,
-    http::{Request, StatusCode},
-    response::IntoResponse,
-    routing::get,
-    Router,
-};
-use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+use axum::{body::Body, http::Request, response::IntoResponse, routing::get, Router};
 use serde_json::json;
 
 pub fn create_route() -> Router<AppState> {
@@ -17,36 +10,13 @@ pub fn create_route() -> Router<AppState> {
 
 async fn verify_jwt(req: Request<Body>) -> impl IntoResponse {
     let headers = req.headers();
-    let authorization = headers.get("Authorization");
-
-    let response_json = match authorization {
-        Some(auth_header) => {
-            let auth_header = auth_header.to_str().unwrap_or("");
-            if !auth_header.starts_with("Bearer ") {
-                (
-                    StatusCode::BAD_REQUEST,
-                    "Authorization header must start with Bearer",
-                )
-            } else {
-                let token = auth_header.trim_start_matches("Bearer ");
-                match decode::<Claims>(
-                    token,
-                    &DecodingKey::from_secret(SECRET_KEY.as_ref()),
-                    &Validation::new(Algorithm::HS256),
-                ) {
-                    Ok(_) => (StatusCode::OK, "Valid token"),
-                    Err(_) => (StatusCode::UNAUTHORIZED, "Invalid token"),
-                }
-            }
-        }
-        None => (StatusCode::BAD_REQUEST, "Authorization header is required"),
-    };
+    let result = jwts::verify_jwt(headers, SECRET_KEY.as_str()).await;
 
     (
-        response_json.0,
+        result.0,
         [("content-type", "application/json")],
         json!({
-            "detail": response_json.1
+            "detail": result.1
         })
         .to_string(),
     )
---
+++ src/services/jwts.rs
@@ -0,0 +1,38 @@
+use crate::utils::jwt::Claims;
+use axum::http::{HeaderMap, HeaderValue, StatusCode};
+use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
+
+pub async fn verify_jwt(headers: &HeaderMap, secret: &'static str) -> (StatusCode, &'static str) {
+    let auth = headers.get("Authorization");
+    let result = match auth {
+        Some(auth_header) => verify_auth_header(auth_header, secret),
+        None => (StatusCode::BAD_REQUEST, "Authorization header is required"),
+    };
+
+    result
+}
+
+fn verify_auth_header(
+    auth_header: &HeaderValue,
+    secret: &'static str,
+) -> (StatusCode, &'static str) {
+    let auth_header_str = auth_header.to_str().unwrap_or("");
+
+    if !auth_header_str.starts_with("Bearer ") {
+        return (
+            StatusCode::BAD_REQUEST,
+            "Authorization header must start with Bearer",
+        );
+    }
+
+    let token = auth_header_str.trim_start_matches("Bearer ");
+
+    match decode::<Claims>(
+        token,
+        &DecodingKey::from_secret(secret.as_ref()),
+        &Validation::new(Algorithm::HS256),
+    ) {
+        Ok(_) => (StatusCode::OK, "Valid token"),
+        Err(_) => (StatusCode::UNAUTHORIZED, "Invalid token"),
+    }
+}
