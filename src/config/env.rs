723e67ad4cfca1e7dec5d26248ac5a42bccf62e0
Hju Kneyck Flores <hju.halcyondigital@gmail.com>
2023-02-23 14:13:10 +0800

    fix: load env vars from .env or system env

--- src/config/env.rs
+++ src/config/env.rs
@@ -1,9 +1,20 @@
 use lazy_static::lazy_static;
+use std::env;
 
 lazy_static! {
-    pub static ref SECRET_KEY: String = dotenv_codegen::dotenv!("SECRET_KEY").to_owned();
-    pub static ref REDIS_URL: String = dotenv_codegen::dotenv!("REDIS_URL").to_owned();
-    pub static ref SMS_HOST: String = dotenv_codegen::dotenv!("SMS_HOST").to_owned();
-    pub static ref DATABASE_URL: String = dotenv_codegen::dotenv!("DATABASE_URL").to_owned();
-    pub static ref APP_NAME: String = dotenv_codegen::dotenv!("APP_NAME").to_owned();
+    pub static ref SECRET_KEY: String = env_or_default("SECRET_KEY");
+    pub static ref REDIS_URL: String = env_or_default("REDIS_URL");
+    pub static ref SMS_HOST: String = env_or_default("SMS_HOST");
+    pub static ref DATABASE_URL: String = env_or_default("DATABASE_URL");
+    pub static ref APP_NAME: String = env_or_default("APP_NAME");
+}
+
+fn env_or_default(key: &str) -> String {
+    match dotenvy::var(key) {
+        Ok(val) => val,
+        Err(_) => match env::var(key) {
+            Ok(val) => val,
+            Err(_) => panic!("{} environment variable not found", key),
+        },
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod app;
 pub mod config;
 pub mod models;
 pub mod routes;
@@ -5,12 +6,17 @@ pub mod services;
 pub mod structs;
 pub mod utils;
 
+use std::env;
 use std::net::SocketAddr;
-
-mod app;
 
 #[tokio::main]
 async fn main() {
+    // Load environment variables from .env file if it exists
+    if let Err(_) = dotenvy::dotenv() {
+        // If .env file does not exist, load environment variables from system environment
+        let _ = env::vars();
+    }
+
     let app = app::create_app().await;
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
