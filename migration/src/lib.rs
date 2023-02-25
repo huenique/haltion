9ba56bbcfa9db68a6397c59fc0d1a5d180e0ba9e
Hju Kneyck Flores <hjucode@gmail.com>
2023-02-25 17:52:28 +0800

    docs: add further info regarding implementations & examples

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,12 +2,31 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20230130_073549_create_employees_table;
 mod m20230209_110348_create_otps_table;
+mod m20230225_062319_create_users_table;
+mod m20230225_062404_create_access_groups_table;
+mod m20230225_062412_create_permissions_table;
+mod m20230225_064211_create_users_to_group;
+mod m20230225_064224_create_users_to_groups;
+mod m20230225_064438_user_groups;
+mod m20230225_064453_create_user_groups_table;
+mod m20230225_064502_create_group_permissions_table;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20230209_110348_create_otps_table::Migration)]
+        vec![
+            Box::new(m20230130_073549_create_employees_table::Migration),
+            Box::new(m20230209_110348_create_otps_table::Migration),
+            Box::new(m20230225_062319_create_users_table::Migration),
+            Box::new(m20230225_062404_create_access_groups_table::Migration),
+            Box::new(m20230225_062412_create_permissions_table::Migration),
+            Box::new(m20230225_064211_create_users_to_group::Migration),
+            Box::new(m20230225_064224_create_users_to_groups::Migration),
+            Box::new(m20230225_064438_user_groups::Migration),
+            Box::new(m20230225_064453_create_user_groups_table::Migration),
+            Box::new(m20230225_064502_create_group_permissions_table::Migration),
+        ]
     }
 }
